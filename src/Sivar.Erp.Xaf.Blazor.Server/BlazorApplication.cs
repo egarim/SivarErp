c4474da8b61b921b30591ba51b3ebe844253fe96
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-08-09 01:55:55 +0300

    xaf

---
+++ src/Sivar.Erp.Xaf.Blazor.Server/BlazorApplication.cs
@@ -0,0 +1,56 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Blazor;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Security.ClientServer;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Xpo;
+using Sivar.Erp.Xaf.Blazor.Server.Services;
+using DevExpress.ExpressApp.MultiTenancy;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sivar.Erp.Xaf.Blazor.Server;
+
+public class XafBlazorApplication : BlazorApplication {
+    public XafBlazorApplication() {
+        ApplicationName = "Sivar.Erp.Xaf";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        DatabaseVersionMismatch += XafBlazorApplication_DatabaseVersionMismatch;
+    }
+    protected override void OnSetupStarted() {
+        base.OnSetupStarted();
+#if DEBUG
+        if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
+        }
+#endif
+    }
+    private void XafBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached || TenantId != null) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+            string message = "The application cannot connect to the specified database, " +
+                "because the database doesn't exist, its version is older " +
+                "than that of the application or its schema does not match " +
+                "the ORM data model structure. To avoid this error, use one " +
+                "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+            if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+            }
+            throw new InvalidOperationException(message);
+        }
+#endif
+    }
+    Guid? TenantId {
+        get {
+            return ServiceProvider?.GetService<ITenantProvider>()?.TenantId;
+        }
+    }
+}
