096eae60d80e090f076e0e9e7f3e3a7339e53adc
Jose Ojeda <joche.ojeda@bitframeworks.com>
2025-08-28 09:20:03 -0600

    tree list

--- src/Sivar.Erp.EfCore/Entities/Account.cs
+++ src/Sivar.Erp.EfCore/Entities/Account.cs
@@ -1,17 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+using System.Collections.Specialized;
 using DevExpress.Persistent.Base;
+using DevExpress.Persistent.Base.General;
+using DevExpress.Data.Filtering;
 using Sivar.Erp.Core.Enums;
 using Sivar.Erp.Modules.Accounting.ChartOfAccounts;
 
 namespace Sivar.Erp.EfCore.Entities;
 
 [DefaultClassOptions()]
+[NavigationItem("Accounting")]
 /// <summary>
 /// Entity Framework entity for Chart of Accounts
 /// </summary>
 [Table("Accounts")]
-public class Account : BaseEntity, IAccount
+public class Account : BaseEntity, IAccount, ITreeNode
 {
     /// <summary>
     /// Optional reference to balance sheet or income statement line
@@ -42,4 +47,68 @@ public class Account : BaseEntity, IAccount
     /// </summary>
     [MaxLength(50)]
     public virtual string? ParentOfficialCode { get; set; }
+
+    #region ITreeNode Implementation
+
+    private IBindingList? _children;
+
+    /// <summary>
+    /// Gets the child accounts of this account
+    /// </summary>
+    [NotMapped]
+    [Browsable(false)]
+    public IBindingList Children
+    {
+        get
+        {
+            if (_children == null && ObjectSpace != null)
+            {
+                // Use ObjectSpace to query child accounts
+                var criteria = CriteriaOperator.Parse("ParentOfficialCode = ?", this.OfficialCode);
+                var childAccounts = ObjectSpace.GetObjects<Account>(criteria);
+                _children = new BindingList<Account>(childAccounts.ToList());
+                
+                // Subscribe to collection changes to invalidate cache
+                if (_children is INotifyCollectionChanged notifyCollection)
+                {
+                    notifyCollection.CollectionChanged += (s, e) => _children = null;
+                }
+            }
+            return _children ?? new BindingList<Account>();
+        }
+    }
+
+    /// <summary>
+    /// Gets the parent account of this account
+    /// </summary>
+    [NotMapped]
+    [Browsable(false)]
+    public ITreeNode? Parent
+    {
+        get
+        {
+            if (string.IsNullOrEmpty(ParentOfficialCode) || ObjectSpace == null)
+                return null;
+
+            var criteria = CriteriaOperator.Parse("OfficialCode = ?", this.ParentOfficialCode);
+            return ObjectSpace.FindObject<Account>(criteria);
+        }
+    }
+
+    /// <summary>
+    /// Gets the display name for the tree node
+    /// </summary>
+    [NotMapped]
+    [Browsable(false)]
+    string ITreeNode.Name => AccountName;
+
+    /// <summary>
+    /// Invalidates the children cache to force refresh
+    /// </summary>
+    public void InvalidateChildrenCache()
+    {
+        _children = null;
+    }
+
+    #endregion
 }
